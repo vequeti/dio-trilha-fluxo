5d2dd59df74ae457496c51640f9bf814a2e8c4e3
Vinicius Vequeti <viniciusvequeti@gmail.com>
2023-09-15 11:44:30 -0300

    Project finished

--- src/application/Contador.java
+++ src/application/Contador.java
@@ -1,9 +1,36 @@
 package application;
 
+import java.util.Scanner;
+
+import exceptions.ParametrosInvalidosException;
+
 public class Contador {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametroUm = sc.nextInt();
+		System.out.println("Digite o segundo parâmetro: ");
+		int parametroDois = sc.nextInt();
+		
+		try {
+			contagem(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException e){
+			System.out.println(e.getMessage());
+		}
 
+		sc.close();
+	}
+	
+	static void contagem(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parâmetro precisa ser maior que o primeiro parâmentro.");
+		}
+		
+		int y = parametroDois - parametroUm;
+		for(int x=1;x<=y;x++) {
+			System.out.println("Imprimindo o número " + x);
+		}
+	}
 }
---
+++ src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException (String msg) {
+		super(msg);
+	}
+}
